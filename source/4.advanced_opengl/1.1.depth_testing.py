b35304b9293a3d0a832ee44625abc120dc1b192f
ranjian0 <karanjaichungwa@gmail.com>
2023-02-01 10:36:43 +0300

    start depth_testing

---
+++ source/4.advanced_opengl/1.1.depth_testing.py
@@ -0,0 +1,192 @@
+import sys
+import glfw
+import OpenGL.GL as gl
+from pathlib import Path
+from pyrr import Vector3, Matrix44
+from ctypes import c_float, sizeof, c_void_p
+
+CURDIR = Path(__file__).parent.absolute()
+sys.path.append(str(CURDIR.parent))
+
+from shader import Shader
+from camera import Camera, CameraMovement
+
+# -- settings
+SRC_WIDTH = 800
+SRC_HEIGHT = 600
+
+# -- camera
+camera = Camera(Vector3([0.0, 0.0, 3.0]))
+last_x = SRC_WIDTH / 2
+last_y = SRC_HEIGHT / 2
+first_mouse = True
+
+# -- timing
+delta_time = 0.0
+last_frame = 0.0
+
+
+def main():
+    global delta_time, last_frame
+
+    if not glfw.init():
+        raise ValueError("Failed to initialize glfw")
+
+    glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 3)
+    glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3)
+
+    glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
+
+    window = glfw.create_window(SRC_WIDTH, SRC_HEIGHT, "learnOpenGL", None, None)
+    if not window:
+        glfw.terminate()
+        raise ValueError("Failed to create window")
+
+    glfw.make_context_current(window)
+    glfw.set_framebuffer_size_callback(window, framebuffer_size_callback)
+    glfw.set_cursor_pos_callback(window, mouse_callback)
+    glfw.set_scroll_callback(window, scroll_callback)
+
+    glfw.set_input_mode(window, glfw.CURSOR, glfw.CURSOR_DISABLED)
+
+    gl.glEnable(gl.GL_DEPTH_TEST)
+    gl.glDepthFunc(gl.GL_ALWAYS)
+
+    shader = Shader(CURDIR / "shaders/1.1.depth_testing.vs", CURDIR / "shaders/1.1.depth_testing.fs")
+
+    vertices = [
+        -0.5, -0.5, -0.5,
+         0.5, -0.5, -0.5,
+         0.5,  0.5, -0.5,
+         0.5,  0.5, -0.5,
+        -0.5,  0.5, -0.5,
+        -0.5, -0.5, -0.5,
+
+        -0.5, -0.5,  0.5,
+         0.5, -0.5,  0.5,
+         0.5,  0.5,  0.5,
+         0.5,  0.5,  0.5,
+        -0.5,  0.5,  0.5,
+        -0.5, -0.5,  0.5,
+
+        -0.5,  0.5,  0.5,
+        -0.5,  0.5, -0.5,
+        -0.5, -0.5, -0.5,
+        -0.5, -0.5, -0.5,
+        -0.5, -0.5,  0.5,
+        -0.5,  0.5,  0.5,
+
+         0.5,  0.5,  0.5,
+         0.5,  0.5, -0.5,
+         0.5, -0.5, -0.5,
+         0.5, -0.5, -0.5,
+         0.5, -0.5,  0.5,
+         0.5,  0.5,  0.5,
+
+        -0.5, -0.5, -0.5,
+         0.5, -0.5, -0.5,
+         0.5, -0.5,  0.5,
+         0.5, -0.5,  0.5,
+        -0.5, -0.5,  0.5,
+        -0.5, -0.5, -0.5,
+
+        -0.5,  0.5, -0.5,
+         0.5,  0.5, -0.5,
+         0.5,  0.5,  0.5,
+         0.5,  0.5,  0.5,
+        -0.5,  0.5,  0.5,
+        -0.5,  0.5, -0.5,
+    ]
+    vertices = (c_float * len(vertices))(*vertices)
+
+    cube_vao = gl.glGenVertexArrays(1)
+    vbo = gl.glGenBuffers(1)
+
+    gl.glBindBuffer(gl.GL_ARRAY_BUFFER, vbo)
+    gl.glBufferData(gl.GL_ARRAY_BUFFER, sizeof(vertices), vertices, gl.GL_STATIC_DRAW)
+
+    gl.glBindVertexArray(cube_vao)
+
+    # -- position attribute
+    gl.glVertexAttribPointer(0, 3, gl.GL_FLOAT, gl.GL_FALSE, 3 * sizeof(c_float), c_void_p(0))
+    gl.glEnableVertexAttribArray(0)
+
+
+    while not glfw.window_should_close(window):
+        # -- time logic
+        current_frame = glfw.get_time()
+        delta_time = current_frame - last_frame
+        last_frame = current_frame
+
+        # -- input
+        process_input(window)
+
+        # -- render
+        gl.glClearColor(0.1, 0.1, 0.1, 1.0)
+        gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
+
+
+        # -- view.projection transformations
+        shader.use()
+        projection = Matrix44.perspective_projection(camera.zoom, SRC_WIDTH/SRC_HEIGHT, 0.1, 100.0)
+        view = camera.get_view_matrix()
+        shader.set_mat4("projection", projection)
+        shader.set_mat4("view", view)
+
+        # -- world transformation
+        model = Matrix44.identity()
+        shader.set_mat4("model", model)
+
+        # -- render cube
+        gl.glBindVertexArray(cube_vao)
+        gl.glDrawArrays(gl.GL_TRIANGLES, 0, 36)
+
+
+        glfw.swap_buffers(window)
+        glfw.poll_events()
+
+    gl.glDeleteVertexArrays(1, id(cube_vao))
+    gl.glDeleteBuffers(1, id(vbo))
+    glfw.terminate()
+
+
+def process_input(window):
+    if glfw.get_key(window, glfw.KEY_ESCAPE) == glfw.PRESS:
+        glfw.set_window_should_close(window, True)
+
+    if glfw.get_key(window, glfw.KEY_W) == glfw.PRESS:
+        camera.process_keyboard(CameraMovement.FORWARD, delta_time)
+    if glfw.get_key(window, glfw.KEY_S) == glfw.PRESS:
+        camera.process_keyboard(CameraMovement.BACKWARD, delta_time)
+
+    if glfw.get_key(window, glfw.KEY_A) == glfw.PRESS:
+        camera.process_keyboard(CameraMovement.LEFT, delta_time)
+    if glfw.get_key(window, glfw.KEY_D) == glfw.PRESS:
+        camera.process_keyboard(CameraMovement.RIGHT, delta_time)
+
+
+def framebuffer_size_callback(window, width, height):
+    gl.glViewport(0, 0, width, height)
+
+
+def mouse_callback(window, xpos, ypos):
+    global first_mouse, last_x, last_y
+
+    if first_mouse:
+        last_x, last_y = xpos, ypos
+        first_mouse = False
+
+    xoffset = xpos - last_x
+    yoffset = last_y - ypos  # XXX Note Reversed (y-coordinates go from bottom to top)
+    last_x = xpos
+    last_y = ypos
+
+    camera.process_mouse_movement(xoffset, yoffset)
+
+
+def scroll_callback(window, xoffset, yoffset):
+    camera.process_mouse_scroll(yoffset)
+
+
+if __name__ == '__main__':
+    main()
